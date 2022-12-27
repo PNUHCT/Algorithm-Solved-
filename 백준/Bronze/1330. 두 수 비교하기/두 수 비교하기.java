a6be03333bf176a37764cd74b7da88b90f0867e5
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-27 20:14:54 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 콘솔 입력용 리터럴. Scanner와 유사함. 단, Buffer특성과 String으로 고정 형변화이라는 특징으로 조금더 빠를수 있음.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // String 형식의 입력값에서 엔터 기준으로 String값을 내보내주는 역할
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if (a>b) System.out.println(">");
+        else if (a<b) System.out.println("<");
+        else System.out.println("==");
+        
+    }
+}
