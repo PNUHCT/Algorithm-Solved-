0900f3350241fb7db1dfc2dbc989ad2e8e3fb366
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-27 22:49:31 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 144 ms, Memory: 14628 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for(int i=1; i<=T; i++) {
+            String star = "";
+            for(int j=0; j<i ; j++){
+                star += "*";
+            }
+            bw.write(star+"\n");
+        }
+
+        bw.close();
+    }
+}
