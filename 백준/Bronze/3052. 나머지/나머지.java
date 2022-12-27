2c5529c7e6722f395f7d5c907d89b303c2788550
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-28 00:48:11 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -5,14 +5,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+       
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<10 ; i++) {
             int temp = Integer.parseInt(br.readLine());
             if(!list.contains(temp%42)) list.add(temp%42);
         }
-        bw.write(""+list.size()+"\n");
-        bw.close();  // stream을 flush 하고, stream을 닫음
+        System.out.println(list.size());
     }
 }
