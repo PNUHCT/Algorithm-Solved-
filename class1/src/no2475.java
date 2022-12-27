57521820bb1bf31814b05480c3c60dc54f353ebf
Dongjin Kim <nyong9221@gmail.com>
2022-12-28 00:14:21 +0900

    Solved: Baekjoon no.2475

---
+++ class1/src/no2475.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class no2475 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int total = 0;
+        for(int i=0; i<5 ; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            total += temp * temp;
+        }
+        System.out.println(total%10);
+    }
+}
+
