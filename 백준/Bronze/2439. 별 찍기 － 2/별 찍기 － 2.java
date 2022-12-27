0dfc6a8cac22b4c7b2dbe90bebc0e3fdb5d0652d
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-27 23:20:49 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 160 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        String[][] matrix = new String[T+1][T];
+
+        for(int i=1; i<=T; i++) {
+
+            for(int j=0; j<T-i; j++) {
+                matrix[i][j] = " ";
+            }
+
+            for(int j=1; j<=i; j++) {
+                matrix[i][T-j] = "*";
+            }
+
+            String S = "";
+            for(int j=0; j<T; j++) {
+                S += matrix[i][j];
+            }
+            System.out.println(S);
+
+        }
+    }
+
+}
+
