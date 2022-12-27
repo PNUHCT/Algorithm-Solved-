c5c40e8d5464786b7709603fae8cfe8241ef5e37
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-28 00:37:33 +0900

    [Bronze II] Title: OX퀴즈, Time: 168 ms, Memory: 16472 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T ; i++) {
+            String[] OX = br.readLine().split("");
+            int total = 0;
+            int point = 0;
+            for(int j=0; j<OX.length; j++) {
+                if(OX[j].equals("O")) {
+                    point++;
+                    total += point;
+                }
+                else if(OX[j].equals("X")) {
+                    point = 0;
+                }
+            }
+            bw.write(""+total+"\n");
+        }
+
+        bw.close();
+    }
+}
