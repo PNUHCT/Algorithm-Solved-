fb101cb08e9aa5beaa2af7b35f9c205993978a88
Dongjin Kim <nyong9221@gmail.com>
2022-12-27 20:07:47 +0900

    Refactor: template -> Main

--- class1/src/Main.java
+++ class1/src/Main.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class template {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         // 콘솔 입력용 리터럴. Scanner와 유사함. 단, Buffer특성과 String으로 고정 형변화이라는 특징으로 조금더 빠를수 있음.
