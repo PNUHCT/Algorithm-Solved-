296542093e02e42f2a513b9f21ba6d1180d97e0d
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-29 01:56:57 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 132 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,73 +4,43 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        if(N<8 || M<8) {
-            bw.write(""+0+"\n");
-            bw.close();
-        }
+        int row = Integer.parseInt(st.nextToken()); // 열
+        int col = Integer.parseInt(st.nextToken()); // 행
 
-        List<String> Atype = new ArrayList<>();
-        List<String> Btype = new ArrayList<>();
-        for(int i=1; i<=8 ; i++) {
-            String BW = "BWBWBWBW";
-            String WB = "WBWBWBWB";
-            if(i%2==1) {
-                Atype.add(BW);
-                Btype.add(WB);
-            } else if (i%2==0) {
-                Atype.add(WB);
-                Btype.add(BW);
+        String[] board = new String[row];
+        for(int i = 0; i < row; i++) board[i] = (br.readLine());
+
+        int min = Integer.MAX_VALUE; // 최소값 찾기 위한 최대값
+
+        // 시작 칸이 될 수 있는 범위까지의 for문
+        for(int i = 0; i <= row - 8; i++) {
+            for(int j = 0; j <= col - 8; j++) {
+                int sol = solved(i, j, board);
+                if(min > sol) min = sol;
             }
         }
-
-        String[][] data = new String[N][M];
-        for (int i = 0; i < N; i++) {
-            String temp = br.readLine();
-            for (int j = 0; j < M; j++) {
-                data[i][j] = String.valueOf(temp.charAt(j));
-            }
-        }
-
-        int min = 64;
-        for(int i=0; i<=N-8 ; i++) {
-            for(int j=0 ; j<=M-8 ; j++) {
-                min = compare(Atype, Btype, data, i, j, min);
-            }
-        }
-
-        bw.write(""+min+"\n");
-        bw.close();
+        System.out.println(min);
     }
 
-    private static int compare (List<String> Atype, List<String> Btype, String[][] data, int low, int col, int min) {
-        int countA = 0;
-        int countB = 0;
-        int result = 0;
-        
-        for(int i=low; i<low+8; i++){
-            for(int j=col; j<col+8; j++) {
-                if(!data[i][j].equals(String.valueOf(Atype.get(i-low).charAt(j-col)))) countA++;
+    // 8 * 8
+    private static int solved(int startRow, int startCol, String[] board) {
+        String[] stanBoard = {"BWBWBWBW", "WBWBWBWB"}; // 기준
+        int sol = 0;
+
+        // 각 칸마다 비교하는 for문
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(board[startRow + i].charAt(startCol + j)
+                        != stanBoard[(startRow + i) % 2].charAt(j)) sol++;
             }
         }
 
-        for(int i=low; i<low+8; i++){
-            for(int j=col; j<col+8; j++) {
-                if(!data[i][j].equals(String.valueOf(Btype.get(i-low).charAt(j-col)))) countB++;
-            }
-        }
-
-        if(countA>=min && countB>=min) result = min;
-        else if(countA<countB) result = countA;
-        else if(countB<=countA) result = countB;
-        
-        return result;
+        // 최대 경우의 수 : 64
+        // 첫 색을 기준으로 센 수, 첫 색의 반대를 기준으로 센 수
+        return Math.min(sol, 64 - sol);
     }
 }
