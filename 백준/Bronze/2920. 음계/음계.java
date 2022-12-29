7e210471dddec8d32dcbc6dbb4d189717784aec3
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-29 11:49:32 +0900

    [Bronze II] Title: 음계, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -5,13 +5,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String code = br.readLine().replace(" ","");
-        if (code.equals("12345678")) bw.write("ascending");
-        else if (code.equals("87654321")) bw.write("descending");
-        else bw.write("mixed");
-
-        bw.close(); 
+        if (code.equals("12345678")) System.out.println("ascending");
+        else if (code.equals("87654321")) System.out.println("descending");
+        else System.out.println("mixed");
+        
     }
 }
