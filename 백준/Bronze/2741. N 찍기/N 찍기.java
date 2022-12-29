70848317404a0042b0172de8fa3b5863e7f0a48e
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-30 02:42:30 +0900

    [Bronze V] Title: N 찍기, Time: 972 ms, Memory: 30444 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=T ; i++) {
+            System.out.println(i);
+        }
+    }
+}
