eb2153f57d61db1f5ba632f698f8bc31b20a341f
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-30 02:55:00 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -5,13 +5,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        if (T%400==0) bw.write(""+1+"\n");
-        else if(T%4==0 && T%100!=0) bw.write(""+1+ "\n");
-        else bw.write(""+0+ "\n");
+        if (T%400==0) System.out.println(1);
+        else if(T%4==0 && T%100!=0) System.out.println(1);
+        else System.out.println(0);
 
-        bw.close();
     }
 }
