63e62556b407dc8f01018590c3a273fb1177160e
Dongjin Kim <nyong9221@gmail.com>
2022-12-31 00:24:23 +0900

    Solved: baekjoon no1874 [스택 수열]

---
+++ class1/src/no1874.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Stack;
+
+public class no1874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        boolean no = false;
+        int count = 0;
+        for(int i = 0; i < T; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if(stack.empty()) {
+                count++;
+                stack.push(count);
+                sb.append("+").append("\n");
+            }
+
+            while(stack.peek()!=num && count<=T) {
+                count++;
+                stack.push(count);
+                sb.append("+").append("\n");
+            }
+
+            if(stack.peek()!=num && count>T) {
+                no = true;
+                break;
+            }
+
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+
+        if(no) {
+            System.out.println("NO");
+        } else {
+            System.out.println(sb);
+        }
+    }
+}
