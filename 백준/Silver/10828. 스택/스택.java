0f6dce8a20754a4f33dc8ccea1b09bd0d70bd4a1
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-12-31 20:32:14 +0900

    [Silver IV] Title: 스택, Time: 224 ms, Memory: 18616 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String cmd = st.nextToken();
+            command(cmd, stack, st, bw);
+        }
+        bw.close();
+    }
+
+    private static void command(String cmd, Stack<String> stack, StringTokenizer st, BufferedWriter bw) throws IOException {
+        if (cmd.equals("push")) {
+            stack.push(st.nextToken());
+        } else if (cmd.equals("top")) {
+            if(stack.isEmpty()) bw.write("-1"+"\n");
+            else bw.write(stack.peek() + "\n");
+        } else if (cmd.equals("size")) {
+            bw.write(stack.size() + "\n");
+        } else if (cmd.equals("empty")) {
+            if (stack.size() == 0) bw.write("1" + "\n");
+            else bw.write("0" + "\n");
+        } else if (cmd.equals("pop")) {
+            if (stack.isEmpty()) bw.write("-1" + "\n");
+            else bw.write(stack.pop() + "\n");
+        }
+    }
+}
+
