e092c77541ad07e52ddb2dec8faaffb06a3e74ff
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-03 11:23:03 +0900

    [Silver IV] Title: 큐, Time: 236 ms, Memory: 19820 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,51 +1,43 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
         Deque<Integer> que = new ArrayDeque<>();
-        for(int i=0; i<T ; i++) {
+        for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            command(que, st);
+            command(que, st, bw);
         }
+
+        bw.close();
     }
 
-    private static void command(Deque<Integer> que, StringTokenizer st) {
+    private static void command(Deque<Integer> que, StringTokenizer st, BufferedWriter bw) throws IOException {
         String cmd = st.nextToken();
-        if(cmd.equals("push")) {
+        if (cmd.equals("push")) {
             int X = Integer.parseInt(st.nextToken());
             que.add(X);
         } else if (cmd.equals("pop")) {
-            if (que.isEmpty()) {
-                System.out.println(-1);
-            } else {
-                System.out.println(que.poll());
-            }
+            if (que.isEmpty()) bw.write("-1\n");
+            else bw.write(""+que.poll()+"\n");
         } else if (cmd.equals("size")) {
-            System.out.println(que.size());
+            bw.write(""+que.size()+"\n");
         } else if (cmd.equals("empty")) {
-            if (que.isEmpty()) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+            if (que.isEmpty()) bw.write("1\n");
+            else bw.write("0\n");
         } else if (cmd.equals("front")) {
-            if(que.isEmpty()) {
-                System.out.println(-1);
-            } else {
-                System.out.println(que.peekFirst());
-            }
+            if (que.isEmpty()) bw.write("-1\n");
+            else bw.write(""+que.peekFirst()+"\n");
         } else if (cmd.equals("back")) {
-            if(que.isEmpty()) {
-                System.out.println(-1);
-            } else {
-                System.out.println(que.peekLast());
-            }
+            if (que.isEmpty()) bw.write("-1\n");
+            else bw.write(""+que.peekLast()+"\n");
         }
     }
 }
-
