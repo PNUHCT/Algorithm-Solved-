c603358fbe3e9be228cef235f5515eb0fd4bb0c3
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-04 12:24:39 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -2,25 +2,16 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         Integer A = Integer.parseInt(st.nextToken());
         Integer B = Integer.parseInt(st.nextToken());
         Integer V = Integer.parseInt(st.nextToken());
-//        int result = (A - B == 1) ? ((V - A) / (A - B)) + 1 : ((V - A) % (A - B)) + 1;
-        int result = 0;
-        if(A - B == 1) {
-            result = ((V - A) / (A - B)) + 1;
-        } else {
-            if((V - A)%(A - B)==0) {
-                result = ((V - A) / (A - B)) + 1;
-            } else {
-                result = ((V - A) / (A - B)) + 2;
-            }
-        }
+
+        int caseA = ((V - A) / (A - B)) + 1;
+        int caseB = ((V - A) / (A - B)) + 2;
+        int result = (A - B == 1) ? caseA : ((V - A)%(A - B)==0) ? caseA : caseB;
 
         System.out.println(result);
     }
