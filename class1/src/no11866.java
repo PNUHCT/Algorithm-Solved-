2c111137f8865d9f12f23481177de3ae77c28a26
Dongjin Kim <nyong9221@gmail.com>
2023-01-05 12:06:12 +0900

    Solved: baekjoon no11866 [요세푸스]

--- class1/src/no11866.java
+++ class1/src/no11866.java
@@ -11,7 +11,6 @@ public class no11866 {
         for(int i=1; i<=T ; i++) dq.add(i);
 
         Integer num = Integer.valueOf(st.nextToken());
-//        String result = "<";
         StringBuilder sb = new StringBuilder();
         sb.append("<");
 
@@ -22,10 +21,8 @@ public class no11866 {
                 count++;
             }
             sb.append(dq.pollFirst());
-//            result += dq.pollFirst();
             if(!dq.isEmpty()) sb.append(", ");
         }
-//        System.out.println(result+">");
         System.out.println(sb.append(">"));
     }
 }
