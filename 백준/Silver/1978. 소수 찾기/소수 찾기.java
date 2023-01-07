da1564fba3084750ee732879f194c44dff111b33
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-08 03:16:35 +0900

    [Silver V] Title: 소수 찾기, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int count = 0;
         for(int i=0; i<T ; i++) {
             int K = Integer.parseInt(st.nextToken());
+            if(K>2 && K%2==0) continue;
             for(int j = 2; j <= K; j++) {
                 if(j==K) {
                     count++;
