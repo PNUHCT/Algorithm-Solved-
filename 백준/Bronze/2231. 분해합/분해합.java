952c414e8403d1364e63efdde4c500bf9da6bfe6
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-08 03:00:32 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,31 +1,31 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
+
+        String str = br.readLine();
+        int N = Integer.parseInt(str);
         int result = 0;
 
-        for(int i = 0; i < N; i++) {
+        // i 는 가능한 최솟값인 N - 9 * N의 각 자릿수부터 시작
+        for(int i = (N - (str.length() * 9)); i < N; i++) {
             int number = i;
-            int sum = 0;
+            int sum = 0;	// 각 자릿수 합 변수
 
             while(number != 0) {
-                sum += number % 10;
+                sum += number % 10;	// 각 자릿수 더하기
                 number /= 10;
             }
-            
+
+            // i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우)
             if(sum + i == N) {
                 result = i;
                 break;
             }
         }
-        bw.write(""+result+"\n");
-
-        bw.close();
+        System.out.println(result);
     }
 }
-
