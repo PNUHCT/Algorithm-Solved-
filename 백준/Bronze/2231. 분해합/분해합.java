d9ceb321fb446c4c4e51668a150e205a5eb74c61
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-07 21:17:29 +0900

    [Bronze II] Title: 분해합, Time: 164 ms, Memory: 16100 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            while(number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+            
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+        bw.write(""+result+"\n");
+
+        bw.close();
+    }
+}
+
