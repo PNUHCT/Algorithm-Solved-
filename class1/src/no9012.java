ff817fa2e3d9bd71bf5b0c1799b04302f0b20fa5
Dongjin Kim <nyong9221@gmail.com>
2023-01-13 01:25:03 +0900

    Solved: baekjoon no9012 [괄호 VPS]

---
+++ class1/src/no9012.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class no9012 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T ; i++) {
+            char arr[] = br.readLine().toCharArray();
+            Deque<Character> dq = new ArrayDeque<>();
+            boolean result= false;
+            for(int j=0 ; j<arr.length ; j++) {
+                if(arr[j]=='(') dq.addLast('(');
+                else if (arr[j]==')' && dq.isEmpty()) {
+                    result = true;
+                    break;
+                }
+                else if (arr[j]==')' && dq.peekLast()=='(') dq.pollLast();
+            }
+            if(!dq.isEmpty()) bw.write("NO\n");
+            else if(result) bw.write("NO\n");
+            else bw.write("YES\n");
+        }
+        bw.close();
+    }
+}
+
