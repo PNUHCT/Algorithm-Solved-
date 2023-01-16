2c068ae46d804f9cb216e4e5cc27e0c62557a3ab
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-17 05:50:54 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 248 ms, Memory: 24316 KB -BaekjoonHub

---
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] strArr = br.readLine().split(" ");
+        int A = first(N, strArr, "1");
+        int B = first(N, strArr, "2");
+
+        int max1 = maxCount(A, "1", N, strArr, 1);
+        int max2 = maxCount(B, "2", N, strArr, 1);
+
+        int result = Math.max(max1, max2);
+        System.out.println(result);
+    }
+
+    private static int first (int N, String[] strArr, String RL) {
+        int num = 0;
+        for(int i = 0; i<N;i++) {
+            if(strArr[i].equals(RL)) {
+                num = i;
+                break;
+            }
+        }
+        return num;
+    }
+
+    private static int maxCount (int A, String goal, int N, String[] strArr, int max) {
+        int count = 0;
+        for (int i=A;i<N;i++) {
+            count = (strArr[i].equals(goal)) ? count+1 : count-1;
+            if(count<0) count=0;
+            if(count>max) max = count;
+        }
+        return max;
+    }
+}
