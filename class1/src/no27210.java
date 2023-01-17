86b374203d51e3b9d7e0fe477a756f7fba008a63
Dongjin Kim <nyong9221@gmail.com>
2023-01-17 22:00:07 +0900

    Solved: baekjoon no27210 [신을 모시는 사당]

---
+++ class1/src/no27210.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no27210 {
+    /**
+     * 예시 1)
+     * 13
+     * 1 1 1 2 2 1 1 1 1 1 1 1 2
+     *
+     * 예시 2)
+     * 13
+     * 1 1 2 2 2 1 1 1 1 1 1 1 2
+     */
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] strArr = br.readLine().split(" ");
+
+        int A = 0, B = 0;
+        if(strArr[0].equals("2")) A = first(N, strArr, "1", 0);
+        else B = first(N, strArr, "2", 0);
+
+        int max1 = maxCount(A, "1", N, strArr, 1, 0);
+        int max2 = maxCount(B, "2", N, strArr, 1, 0);
+
+        int result = Math.max(max1, max2);
+
+        System.out.println(result);
+    }
+
+    private static int first (int N, String[] strArr, String RL, int num) {
+        for(int i = 0; i<N;i++) {
+            if(strArr[i].equals(RL)) {
+                num = i;
+                break;
+            }
+        }
+        return num;
+    }
+
+    private static int maxCount (int A, String goal, int N, String[] strArr, int max, int count) {
+        for (int i=A;i<N;i++) {
+            count = (strArr[i].equals(goal)) ? count+1 : count-1;
+            if(count<0) count=0;
+            if(count>max) max = count;
+        }
+        return max;
+    }
+}
