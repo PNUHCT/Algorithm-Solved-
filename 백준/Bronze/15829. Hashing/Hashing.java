6d742902d762c9aca0a8fa9b04885f1b031e8aa4
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-18 03:25:40 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -5,18 +5,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        br.readLine();
-        double r = 31;
-        double i = 0;
-        long result = 0;
+        int T = Integer.parseInt(br.readLine());
         char[] arr = br.readLine().toCharArray();
-
-        for(Character c:arr) {
-            result += Math.pow(r, i) * (c - 96);
-            i++;
-        }
+        long result;
+        int cnt = T-1;
+        result = calculate(arr, cnt, arr[cnt]-96);
 
         System.out.println(result);
+    }
+    private static long calculate(char[] arr, int cnt, long result) {
+        if(cnt<=0) return result;
+        result = (arr[cnt-1]-96) + (31 * result);
+        cnt--;
+        return calculate(arr, cnt, result);
     }
 }
