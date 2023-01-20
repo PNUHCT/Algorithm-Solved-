bc130dbc24eeb9af10758f3111ba3ee2c2ed46ef
Dongjin Kim <nyong9221@gmail.com>
2023-01-20 15:09:42 +0900

    Solved: baekjoon no2839 [설탕 배달]

---
+++ class1/src/no2839.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no2839 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = -1;
+        for (int i = N / 5; i >= 0; i--) {
+            int K = N - (5 * i);
+            if (K % 3 == 0) {
+                count += i + (K / 3) + 1;
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
