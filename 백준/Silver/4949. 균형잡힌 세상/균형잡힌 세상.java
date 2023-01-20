5e1b9f4e28a871d0fcfd9f262807adce6d7baae8
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-21 03:13:49 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 252 ms, Memory: 22020 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,11 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,46 +10,39 @@ public class Main {
 
         while (true) {
             String str = br.readLine();
-            if (str.equals(".")) break;
-            String[] strArr = str.split("");
-            String result = "yes";
+            if(str.equals(".")) break;
 
-            List<String> list = Arrays.stream(strArr).filter(a -> a.equals("(") || a.equals("[") || a.equals(")") || a.equals("]")).collect(Collectors.toList());
             Deque<String> dq = new ArrayDeque<>();
 
-            for (String bal : list) {
-                if (bal.equals("(") || bal.equals("[")) dq.addLast(bal);
 
-                if (bal.equals(")")) {
+            String result = "yes";
+            for(int i=0 ; i<str.length() ; i++) {
+                if(str.charAt(i)=='(' || str.charAt(i)=='[') dq.addLast(String.valueOf(str.charAt(i)));
+                else if(str.charAt(i)==')') {
                     if (dq.isEmpty()) {
                         result = "no";
                         break;
                     }
-                    else if (dq.peekLast().equals("(")) {
-                        dq.pollLast();
-                    }
-                    else {
-                        result = "no";
-                        break;
+                    else if(dq.peekLast().equals("(")) dq.pollLast();
+                    else if (!dq.peekLast().equals("(")) {
+                            result = "no";
+                            break;
                     }
                 }
-
-                if (bal.equals("]")) {
+                else if(str.charAt(i)==']') {
                     if (dq.isEmpty()) {
                         result = "no";
                         break;
                     }
-                    else if (dq.peekLast().equals("[")) {
-                        dq.pollLast();
-                    }
-                    else {
+                    else if(dq.peekLast().equals("[")) dq.pollLast();
+                    else if (!dq.peekLast().equals("[")) {
                         result = "no";
                         break;
                     }
                 }
             }
             if(!dq.isEmpty()) result= "no";
-            sb.append(result + "\n");
+            sb.append(result+"\n");
         }
 
         System.out.println(sb);
