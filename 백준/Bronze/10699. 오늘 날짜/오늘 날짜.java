81305ad11db06dd164945d1141a11b270f4a7754
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-25 14:49:42 +0900

    [Bronze V] Title: 오늘 날짜, Time: 196 ms, Memory: 17404 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate time = LocalDate.now();
+        System.out.println(time);
+    }
+}
