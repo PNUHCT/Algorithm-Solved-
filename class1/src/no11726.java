7d5d3b3395a460554ddc4b9170ce16b37b7a9b2a
Dongjin Kim <nyong9221@gmail.com>
2023-01-28 00:10:12 +0900

    Solved : no11726 [2xN 타일링]

---
+++ class1/src/no11726.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class no11726 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        System.out.println(tiling(T));
+    }
+
+    public static int tiling(int num) {
+        if(num==1) return 1;
+        int [] arr = new int[num];
+        arr[0] = 1;
+        arr[1] = 2;
+
+        for(int i=2 ; i < num ; i++ ){
+            arr[i] = (arr[i-1] + arr[i-2])%10007;
+        }
+        return arr[num-1];
+    }
+}
+
