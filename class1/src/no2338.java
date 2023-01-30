4bd5a0aca7c833aab12d28470c6f596829dcd541
Dongjin Kim <nyong9221@gmail.com>
2023-01-31 02:37:28 +0900

    Solved : no2338 [긴자리 계산]

---
+++ class1/src/no2338.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class no2338 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
+
+        bw.write(""+A.add(B)+"\n");
+        bw.write(""+A.subtract(B)+"\n");
+        bw.write(""+A.multiply(B)+"\n");
+
+        bw.close();
+    }
+}
+
