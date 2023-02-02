89ebbcaf662be880f5bd24a2d064cb21efcf5b91
Dongjin Kim <nyong9221@gmail.com>
2023-02-03 02:25:49 +0900

    Solved: no11047 [동전 0]

---
+++ class1/src/no11047.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class no11047 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<N ; i++) list.add(Integer.parseInt(br.readLine()));
+        List<Integer> sortedList = list.stream().sorted(Comparator.reverseOrder()).toList();
+        int count = 0;
+        for(int i=0; i<sortedList.size(); i++) {
+            if(sortedList.get(i)<=K) {
+                count += K/sortedList.get(i);
+                K %= sortedList.get(i);
+            }
+        }
+        System.out.println(count);
+    }
+}
+
