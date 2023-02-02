d97abeee8849c3eaf23ea464e7cce3113a0d47d0
Dongjin Kim <nyong9221@gmail.com>
2023-02-02 18:29:37 +0900

    Softeer: Init

---
+++ class1/src/Softeer/MainForSofteer.java
@@ -0,0 +1,26 @@
+package Softeer;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class MainForSofteer {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        BigInteger bigint;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T ; i++) {
+
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        bw.close();
+    }
+}
+
