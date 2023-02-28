8af1a175c1d008c2d4e0e325e87ff9f0178fdf74
Dongjin Kim <nyong9221@gmail.com>
2023-02-28 16:38:34 +0900

    Init: Programmers Template

---
+++ class1/src/Programmers.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Programmers {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
