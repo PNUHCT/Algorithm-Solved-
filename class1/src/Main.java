5faa9a9d50f525ba832cb30732b8792545e4c263
Dongjin Kim <nyong9221@gmail.com>
2023-03-01 16:06:58 +0900

    init : Programmers

--- class1/src/Main.java
+++ class1/src/Main.java
@@ -1,24 +1,19 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BigInteger bigint;
-
-        int T = Integer.parseInt(br.readLine());
-
-        for(int i=0; i<T ; i++) {
-
+        String T = br.readLine();
+        String[] arr = T.split("");
+        String result = "";
+        for(int i=0; i<arr.length ; i++) {
+            String temp = arr[i];
+            if(Character.isUpperCase(T.charAt(i))) result += temp.toLowerCase();
+            else result += temp.toUpperCase();
         }
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        bw.close();
+        System.out.println(result);
     }
 }
 
