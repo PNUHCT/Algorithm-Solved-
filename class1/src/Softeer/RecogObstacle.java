d6a876cd87c798c6ac92b372565b11ae1a02e609
Dongjin Kim <nyong9221@gmail.com>
2023-03-02 16:31:22 +0900

    Softeer: recog-obstacle

---
+++ class1/src/Softeer/RecogObstacle.java
@@ -0,0 +1,19 @@
+package Softeer;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RecogObstacle {
+    public static void main(String[] args) {
+        System.out.println(solution("타이틀"));
+    }
+
+    public static String solution(String title) {
+        String answer = title;
+
+        return answer;
+    }
+}
