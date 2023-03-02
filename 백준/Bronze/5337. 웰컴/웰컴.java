9a801ab09139670848425b701d5f7c9f4a7e1627
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-03-02 11:50:15 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+public class Main {
+    private static String welcome = ".  .   .\n" +
+            "|  | _ | _. _ ._ _  _\n" +
+            "|/\\|(/.|(_.(_)[ | )(/.";
+
+    public static void main (String[] args) {
+        System.out.println(welcome);
+    }
+}
