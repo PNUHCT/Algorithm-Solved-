c23036573ad3362f24fb45a3b364780c5f8a2f15
Novice_Neoguri <nyong9221@gmail.com>
2023-03-15 12:32:17 +0900

    [unrated] Title: 자동차 평균 대여 기간 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
@@ -0,0 +1,11 @@
+-- CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서
+-- 평균 대여 기간이 7일 이상인 자동차들의 
+-- 자동차 ID와 평균 대여 기간(컬럼명: AVERAGE_DURATION) 리스트를 출력
+-- 평균 대여 기간은 소수점 두번째 자리에서 반올림
+-- 결과는 평균 대여 기간을 기준으로 내림차순 정렬
+-- 평균 대여 기간이 같으면 자동차 ID를 기준으로 내림차순 정렬해주세요.
+SELECT CAR_ID, ROUND(AVG(TIMESTAMPDIFF(HOUR,START_DATE,END_DATE)/24+1), 1) AS AVERAGE_DURATION
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+HAVING AVERAGE_DURATION >= 7
+ORDER BY AVERAGE_DURATION DESC, CAR_ID DESC
