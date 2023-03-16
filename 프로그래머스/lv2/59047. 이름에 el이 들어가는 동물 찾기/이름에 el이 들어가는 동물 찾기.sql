9bb0b9ae0cc5d522ec9afef0b06ca5b924e089b1
Novice_Neoguri <nyong9221@gmail.com>
2023-03-16 12:13:43 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,6 +1,15 @@
 -- 이름에 "EL"이 들어가는 개의 아이디와 이름을 조회
 -- 결과는 이름 순으로 조회
 -- 단, 이름의 대소문자는 구분하지 않습니다.
+
+-- WHERE 조건1 : 이름에 "EL"이 대소문자 구분없이 키워드 검색
+  # => LOWER(컬럼명) LIKE LOWER("%키워드%") 
+
+-- WHERE 조건2 : 동물 타입이 개
+  # => 컬럼명 LIKE "키워드"
+  
+-- 정렬조건 : 이름
+  # => ORDER BY 컬럼명
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE LOWER(NAME) LIKE LOWER("%el%") AND ANIMAL_TYPE LIKE "Dog"
