244afffbc0dbe5c7114290aa2b00fd99b7262cad
Novice_Neoguri <nyong9221@gmail.com>
2023-03-22 02:31:40 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,9 @@
+-- 천재지변으로 인해 일부 데이터가 유실되었습니다.
+-- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의
+-- ID와 이름을 
+-- ID 순으로 조회
+SELECT OUTS.ANIMAL_ID, OUTS.NAME
+FROM ANIMAL_INS INS
+RIGHT JOIN ANIMAL_OUTS OUTS ON INS.ANIMAL_ID LIKE OUTS.ANIMAL_ID
+WHERE INS.ANIMAL_ID IS NULL
+ORDER BY OUTS.ANIMAL_ID
