379208718e54270aef3066b8edc9a75b0c334785
Novice_Neoguri <nyong9221@gmail.com>
2023-03-29 15:10:31 +0900

    [Silver III] Title: Four Squares, Time: 176 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -11,28 +11,30 @@ public class Main {
      * 6. 중간에 같은 수가 나오면, 제곱을 위해 더한 개수를 출력
      */
 
-    private static double N;
+    private static double N; // 최종 목표가 될 임의로 들어오는 자연수. static 메소드에서 사용하기 위해 static 변수로 받음
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-
-        System.out.println(brute());
+        N = Integer.parseInt(br.readLine()); // 입력받은 자연수를 static 변수에 할당해주는 구간
+        System.out.println(brute()); // static 메소드를 호출해서 결과 출력
     }
 
+    /**
+     * @return 사용한 숫자의 개수
+     */
     private static int brute () {
-        int result = 4;
-        int sqrtN = (int) Math.sqrt(N);
+        int result = 4; // 3번의 반복을 수행하고도 안되면 나머진 전부 4개 쓴거임
+        int sqrtN = (int) Math.sqrt(N); // 반복은 최대 N의 제곱근까지임. 그 이상은 N을 넘어가므로
 
-        for(int i = 0; i <= sqrtN; i++) {
-            for(int j = 0; j <= sqrtN; j++) {
-                for(int k = 0; k <= sqrtN; k++) {
-                    double sum = Math.pow(k, 2) + Math.pow(j, 2) + Math.pow(i, 2);
-                    if(sum==N) {
-                        if(i!=0) return 3;
-                        else if (j!=0) return 2;
-                        else return 1;
+        // * 주의할 점 : 0부터 N제곱근과 같은 숫자까지 반복해야함. (0은 안쓴경우, first < sqrtN은 sqrtN이 포함 안되므로 주의)
+        for(int third = 0; third <= sqrtN; third++) {
+            for(int second = 0; second <= sqrtN; second++) {
+                for(int first = 0; first <= sqrtN; first++) {
+                    double sum = Math.pow(first, 2) + Math.pow(second, 2) + Math.pow(third, 2);
+                    if(sum==N) {  // 각 제곱의 합이 N에 도달했을 때,
+                        if(third!=0) return 3; // 3개까지 썼을 경우
+                        else if (second!=0) return 2; // 2개까지 썼을 경우
+                        else return 1; // 1개만 썼을 경우
                     }
                 }
             }
