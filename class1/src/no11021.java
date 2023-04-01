3b33291e08ff9ad5b400e95853193cb005d7c91d
Dongjin Kim <nyong9221@gmail.com>
2023-04-01 20:45:54 +0900

    Solved: no11021

---
+++ class1/src/no11021.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class no11021 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=T ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int head = Integer.parseInt(st.nextToken());
+            int tail = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #"+i+": "+ (head+tail)+"\n");
+        }
+
+        bw.close();
+    }
+}
+
