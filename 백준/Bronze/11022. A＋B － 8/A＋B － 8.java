d09a09ed06c343bc1eac1cdde1d53bd0465f4787
Novice_Neoguri <nyong9221@gmail.com>
2023-04-05 00:56:06 +0900

    [Bronze V] Title: A+B - 8, Time: 164 ms, Memory: 14676 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=T ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println(String.format("Case #%d: %d + %d = %d", i, A, B, A+B));
+        }
+    }
+}
