90f802d44ddcc7547b08c8a42fce799602e408e7
Novice_Neoguri <nyong9221@gmail.com>
2023-04-12 01:02:44 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int result = 1;
+        while(T>0) {
+            result *= T;
+            T--;
+        }
+        System.out.println(result);
+    }
+}
