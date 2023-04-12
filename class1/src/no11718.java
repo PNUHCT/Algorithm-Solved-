85778a172b6afdecbb4f12962f98f5881f1c2bc7
Dongjin Kim <nyong9221@gmail.com>
2023-04-12 18:33:30 +0900

    Solved: no11718

---
+++ class1/src/no11718.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class no11718 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line=br.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
