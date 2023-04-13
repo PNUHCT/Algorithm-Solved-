4223d37a088ab5715c2190ac95fb8770a481fcc6
Dongjin Kim <nyong9221@gmail.com>
2023-04-14 02:20:20 +0900

    Solved: no11382

---
+++ class1/src/no11382.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class no11382 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        BigInteger C = new BigInteger(st.nextToken());
+
+        System.out.println(A.add(B).add(C));
+    }
+}
+
