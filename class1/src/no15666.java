8c8b29b16b61d013f2a676a12989e67e1b9d221c
Dongjin Kim <nyong9221@gmail.com>
2023-05-01 12:46:53 +0900

    Solved: no15666 - 재귀(Combination)

---
+++ class1/src/no15666.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class no15666 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int N;
+    private static int M;
+    private static List<Integer> nums = new ArrayList<>();
+    private static int[] subset;
+    private static List<String> results = new ArrayList<>();
+
+
+    public static void main(String[] args) throws IOException {
+        // 입력 받는 구간
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        subset = new int[M];
+
+        StringTokenizer numbers = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) nums.add(Integer.parseInt(numbers.nextToken()));
+        Collections.sort(nums);
+
+        // 재귀 반복 시작
+        Combination(0);
+
+        // 중복 제거
+        results = results.stream().distinct().collect(Collectors.toList());
+
+        // 최종 결과값들 입력
+        for (int i = 0; i < results.size(); i++) bw.write(results.get(i) + "\n");
+
+        bw.close();
+    }
+
+    private static void Combination(int cnt) {
+        // 원하는 만큼 뽑았을 경우
+        if (cnt == M) {
+            // 비내림차순이 아닌경우 통과
+            for (int i = 0; i < M; i++) {
+                for(int j=i; j < M; j++) {
+                    if(subset[i]>subset[j]) return;
+                }
+            }
+
+            // 비내림차순인 경우 결과리스트에 저장
+            String str = "";
+            for (int i = 0; i < M; i++) str += subset[i] + " ";
+            results.add(str);
+            return;
+        }
+
+        // 원하는 만큼 뽑기 전까지의 반복 (중복을 허용하기 위해서 방문체크 visited는 하지 않는다.)
+        for (int i = 0; i < N; i++) {
+                subset[cnt] = nums.get(i);
+                Combination(cnt + 1);
+        }
+    }
+}
