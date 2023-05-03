29aa01f3a221f8d9b58bdf5a549ad928ec001fdb
Dongjin Kim <nyong9221@gmail.com>
2023-05-03 12:32:23 +0900

    Solved: no14681

---
+++ class1/src/no14681.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no14681 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        int Y = Integer.parseInt(br.readLine());
+
+        if(X>0 && Y>0) System.out.println(1);
+        else if (X<0 && Y>0) System.out.println(2);
+        else if (X<0 && Y<0) System.out.println(3);
+        else System.out.println(4);
+    }
+}
