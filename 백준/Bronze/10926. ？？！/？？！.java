3e0fade795dd3064f17d34d40e8ddae12c7c9389
Novice_Neoguri <nyong9221@gmail.com>
2023-05-05 03:52:11 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine()+"??!");
+    }
+}
+
