835ad83a215e4f06150008c4c73c68cf40d7d37b
Novice_Neoguri <nyong9221@gmail.com>
2023-05-07 13:54:56 +0900

    [Bronze V] Title: WARBOY, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        System.out.println(3 * (B/A) * C);
+    }
+}
