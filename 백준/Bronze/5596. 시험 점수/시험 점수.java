40b0dd2ac369d7c2be7b01b388bdf2dd1d85f4a0
Novice_Neoguri <nyong9221@gmail.com>
2023-05-15 23:35:55 +0900

    [Bronze IV] Title: 시험 점수, Time: 120 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr1 = br.readLine().split(" ");
+        String[] arr2 = br.readLine().split(" ");
+
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i=0 ; i<4 ; i++) {
+            sum1 += Integer.parseInt(arr1[i]);
+            sum2 += Integer.parseInt(arr2[i]);
+        }
+
+        int max = sum1>=sum2 ? sum1 : sum2;
+        System.out.println(max);
+    }
+}
