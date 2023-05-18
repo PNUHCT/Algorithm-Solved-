9dfe65454731859998d462bb786258906ec10442
Novice_Neoguri <nyong9221@gmail.com>
2023-05-19 01:08:31 +0900

    Solved: no5717

---
+++ class1/src/no5717.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class no5717 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        while(true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int boy = Integer.parseInt(st.nextToken());
+            int girl = Integer.parseInt(st.nextToken());
+            if(boy==0 && girl==0) break;
+            bw.write(""+(boy + girl)+"\n");
+        }
+
+        bw.close();
+    }
+}
