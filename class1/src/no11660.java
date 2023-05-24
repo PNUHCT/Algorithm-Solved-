fdfb200e53c1bcf2887e44cc7ce0980d632cf6a9
= <=>
2023-05-24 15:16:56 +0900

    no11660 누적합(1중반복)

--- class1/src/no11660.java
+++ class1/src/no11660.java
@@ -3,8 +3,8 @@ import java.util.StringTokenizer;
 
 public class no11660 {
     private static int N, M, x1, x2, y1, y2;
-    // private static int[][] matrix;
     private static int[] arr;
+    private static int[] DP;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,53 +15,7 @@ public class no11660 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-//         // 이중배열로 구하는 방식
-//         matrix = new int[N][N];
-
-//         for (int i = 0; i < N; i++) {
-//             StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-//             for (int j = 0; j < N; j++) {
-//                 matrix[i][j] = Integer.parseInt(st2.nextToken());
-//             }
-//         }
-
-
-//         for (int i = 0; i < M; i++) {
-//             StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-//             x1 = Integer.parseInt(st2.nextToken());
-//             y1 = Integer.parseInt(st2.nextToken());
-//             x2 = Integer.parseInt(st2.nextToken());
-//             y2 = Integer.parseInt(st2.nextToken());
-
-//             bw.write(""+calculate()+"\n");
-//         }
-
-//         bw.close();
-//     }
-//     private static int calculate () {
-//         int sum = 0;
-//         for (int y = y1; y <= y2; y++) {
-//             for (int x = x1; x <= x2; x++) {
-//                 if(x>=x1) sum += matrix[y-1][x-1];
-//             }
-//         }
-//         return sum;
-//     }
-// }
-
         // DP
-
-        /* DP 점화식
-         (1N + x1) + (1N + (x1 + 1)) + (1N + ((x1 + 2)) + ... + (1N + ((x2 - x1 + 1))
-         ...
-         ((y2 - y1 + 1)N + x1) + ((y2 - y1 + 1)N + (x1 + 1)) + ((y2 - y1 + 1)N + ((x1 + 2)) + ... + ((y2 - y1 + 1)N + ((x2 - x1 + 1))
-
-         =
-         for(int i = 1 ; i<= y2 - y1 +1 ; i++) {
-            int sum = factorial(x2 - x1) + ((x1 + N) * (x2 - x1 + 1));
-         }
-        */
-
        arr = new int [N*N+1]; // 매트릭스fmf 1차원 배열로 나열
        int num = 0;
        for (int y = 0; y < N ; y++) {
@@ -81,8 +35,8 @@ public class no11660 {
            y1 = Integer.parseInt(st3.nextToken());
            x2 = Integer.parseInt(st3.nextToken());
            y2 = Integer.parseInt(st3.nextToken());
-
-           bw.write(""+calculate()+"\n");
+           calculate();    
+           bw.write(""+DP[(y2-1)*N+x2]+"\n");
        }
 
        bw.close();
@@ -91,28 +45,34 @@ public class no11660 {
 
     // 1  2  3  4  2  3  4  5  3  4  5  6  4  5  6  7
     // 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16
-   private static int calculate () {
-       int sum = 0;
+   private static void calculate () {
+       DP = new int[N*N+1];
        int num = (N*(y1-1))+x1;
        int finish = (N*(y2-1))+x2;
+       int last = 0;
 
        while(num<=finish) {
-            
             if(num%N>x2) {
                 num += ((N-x2)+x1);
-            } else if (num%N == 0 && x2!=N) { 
+            } else if (num%N == 0 && x2!=N) { // x2와 N이 같은 경우의 엣지케이스
                 num += x1;
             }else {
-                sum += arr[num];
+                DP[num] = DP[last] + arr[num];
+                last = num;
                 num++;
             }
        }
-
-       return sum;
-   }
-
-   private static int factorial (int a) {
-       if(a <= 1) return a;
-       else return factorial(a-1) * a;
    }
 }
+
+
+        /* DP 점화식
+         (1N + x1) + (1N + (x1 + 1)) + (1N + ((x1 + 2)) + ... + (1N + ((x2 - x1 + 1))
+         ...
+         ((y2 - y1 + 1)N + x1) + ((y2 - y1 + 1)N + (x1 + 1)) + ((y2 - y1 + 1)N + ((x1 + 2)) + ... + ((y2 - y1 + 1)N + ((x2 - x1 + 1))
+
+         =
+         for(int i = 1 ; i<= y2 - y1 +1 ; i++) {
+            int sum = factorial(x2 - x1) + ((x1 + N) * (x2 - x1 + 1));
+         }
+        */
