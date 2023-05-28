08591c3439d0927476d5377707b370ac029dcf21
Novice_Neoguri <nyong9221@gmail.com>
2023-05-28 22:08:57 +0900

    Solved: no5524

---
+++ class1/src/no5524.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class no5524 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T ; i++) {
+            String Si = br.readLine().toLowerCase();
+            bw.write(Si+"\n");
+        }
+
+        bw.close();
+    }
+}
