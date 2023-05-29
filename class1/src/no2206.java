d877177d7e4009611338522aee17b131d1a83cb9
= <=>
2023-05-29 13:53:14 +0900

    no2206

--- class1/src/no2206.java
+++ class1/src/no2206.java
@@ -10,13 +10,13 @@ public class no2206 {
      * 4. 만약 1일경우, 능력이 남아 있다면 능력 -1하고 통과
      */
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     private static String[][] map;
     private static boolean[][] visited;
     private static int[] hor = {-1, 1, 0, 0};
     private static int[] ver = {0, 0, -1, 1};
-    private static int N, M, min;
+    private static int N, M;
+    private static int min = 1000001;
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");    
         N = Integer.parseInt(st.nextToken());
@@ -24,11 +24,16 @@ public class no2206 {
 
         map = new String[N][M];
         visited = new boolean[N][M];
-        for(int i=0 ; i<N ; i++) map[i] = br.readLine().split(" ");
+
+        for(int i=0 ; i<N ; i++) {
+            String temp = br.readLine();
+            for (int j=0 ; j<M ; j++) map[i][j] = String.valueOf(temp.charAt(j));
+        }
 
         BFS();
 
-        System.out.println(min);
+        if(min == 1000001) System.out.println(-1);
+        else System.out.println(min);
     }
 
     private static void BFS () {
@@ -40,36 +45,38 @@ public class no2206 {
             Node now = dq.poll();
             int nowX = now.X;
             int nowY = now.Y;
+            visited[nowY][nowX] = true;
 
-            if(nowY == N && nowX == M) {
-                if(now.Step<=min) min = now.Step;
+            if(nowY == N-1 && nowX == M-1) {
+                min = now.Step;
+                break;
             }
 
             for(int i = 0 ; i<4 ; i++) {
                 int nextX = nowX+hor[i];
                 int nextY = nowY+ver[i];
 
-                if(0<=nextX && nextX<M && 0<=nextY && nextY<N & !visited[nextY][nextX]) {
+                if(0<=nextY && nextY<N && 0<=nextX && nextX<M && !visited[nextY][nextX]) {
+
+                    // 1일때
                     if(map[nextY][nextX].equals("1")) {
                         if(now.Chance==1) { // 벽을 부술때
                             dq.add(new Node(nextX, nextY, now.Step+1, 0));
-                        } else { // 벽을 못부술때
-                            dq.add(new Node(nextX, nextY, now.Step+1, 0)); // 이 부분이 살짝 미심쩍 (벽을 부수는 경우)
-                        }
-                    } else if(now.Chance==0) { // map에서 0일 경우 중, 벽 부술 찬스가 없는 경우
+                        } 
+                    } 
+
+                    // 0일때
+                    else if(now.Chance==0) { // map에서 0일 경우 중, 벽 부술 찬스가 없는 경우
                         dq.add(new Node(nextX, nextY, now.Step+1, 0));
                     } else { // map에서 0일 경우 중, 벽 부술 찬스가 있는 경우
                         dq.add(new Node(nextX, nextY, now.Step+1, 1));
                     }
-                    visited[nextY][nextX] = true;
+
+                    // visited[nextY][nextX] = true;
                 }               
             }
         }
     }
-
-
-
-
 
     private static class Node {
         private int X;
