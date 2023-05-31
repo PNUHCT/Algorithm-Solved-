0c40858e1ebf36307fa1838e52c1769572446217
Novice_Neoguri <nyong9221@gmail.com>
2023-06-01 00:29:43 +0900

    Solved: no11404 - 오답

--- class1/src/no11404.java
+++ class1/src/no11404.java
@@ -8,9 +8,6 @@ public class no11404 {
     private static Integer[][] map;
     private static boolean[][] visited;
 
-    /**
-     * 
-     */
     public static void main(String[] args) throws IOException {
         city = Integer.parseInt(br.readLine());
         bus = Integer.parseInt(br.readLine());
@@ -22,10 +19,9 @@ public class no11404 {
         }
         
         /**
-         * 이제 맵마다 순회하며, 각각 도시별로 가는 최소비용을 " "로 구분해서 출력해야한다.
+         * 맵마다 순회하며, 각각 도시별로 가는 최소비용을 " "로 구분해서 출력해야한다.
          * 예를들어, 1번째 줄은 A에서 A, B, C, D, E로 가는 최소 비용을 0 2 3 1 4로 출력하며,
          * 5번째 줄은 E에서 A, B, C, D, E로 가는 최소비용을 7 4 10 6 0으로 출력한다.
-         * DFS 사용?
          */
 
         visited = new boolean[city][city];
