d936dad4b9af6b510ebb5b9eb0f781f75d0db847
Novice_Neoguri <nyong9221@gmail.com>
2023-06-05 20:44:40 +0900

    Solved: no11444 - 피보나치 (행렬)

--- class1/src/no11444.java
+++ class1/src/no11444.java
@@ -1,30 +1,51 @@
 import java.io.*;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
+/**
+ * 재귀를 이용한 계산방식
+ */
 public class no11444 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static List<BigInteger> list = new ArrayList<>();
     private static BigInteger mod = new BigInteger("1000000007");
+    private static BigInteger[][] base = {{new BigInteger("1"), new BigInteger("1")},
+                                            {new BigInteger("1"), new BigInteger("0")}};
 
-    public static void main(String[] args) throws IOException {
-        list.add(new BigInteger("0"));
-        list.add(new BigInteger("0"));
-        list.add(new BigInteger("1"));
+    public static void main(String[] args) throws Exception {
+        BigInteger N = new BigInteger(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+        /** 공식이 될 행렬
+         *        |1   1|     |F(n+1)  F(n)|
+         * A^n =  |     |  =  |            |
+         *        |1   0|     |F(n)  F(n-1)|
+         */
 
-        if(N==2) System.out.println(1);
-        else {
-            for(int i=3; i<=N ; i++) {
-                BigInteger two = list.get(i-2);
-                BigInteger one = list.get(i-1);
-                list.add(i, two.add(one));
-            }
-            System.out.println(list.get(N).mod(mod));
-        }
+        BigInteger[][] A = {{new BigInteger("1"), new BigInteger("1")},
+                            {new BigInteger("1"), new BigInteger("0")}};
+
+        System.out.println(pow(A, N.subtract(new BigInteger("1")))[0][0]);
+    }
+
+    private static BigInteger[][] pow(BigInteger[][] A, BigInteger exp) {
+        if(exp.equals(new BigInteger("1")) || exp.equals(new BigInteger("0"))) return A; // 지수가 1 또는 O인 경우
+
+        BigInteger[][] ret = pow(A, exp.divide(new BigInteger("2"))); // 지수를 절반으로 분할하여 재귀 호출
+
+        ret = multiply(ret,ret);
+
+        if(exp.mod(new BigInteger("2")).equals(new BigInteger("1"))) ret = multiply(ret, base);
+
+        return ret;
+    }
+
+    private static BigInteger[][] multiply(BigInteger[][] o1, BigInteger[][] o2) {
+        BigInteger[][] ret = new BigInteger[2][2];
+
+        ret[0][0] = ((o1[0][0]).multiply(o2[0][0])).add((o1[0][1].multiply(o2[1][0]))).mod(mod);
+        ret[0][1] = ((o1[0][0]).multiply(o2[0][1])).add((o1[0][1].multiply(o2[1][1]))).mod(mod);
+        ret[1][0] = ((o1[1][0]).multiply(o2[0][0])).add((o1[1][1].multiply(o2[1][0]))).mod(mod);
+        ret[1][1] = ((o1[1][0]).multiply(o2[0][1])).add((o1[1][1].multiply(o2[1][1]))).mod(mod);
+
+        return ret;
     }
 }
 
