0e98fd14d85e4ce4ecf20a871fc02437f0b68dc4
Novice_Neoguri <nyong9221@gmail.com>
2023-06-09 23:43:16 +0900

    Solved: no1167 - 트리의지름

---
+++ class1/src/no1167.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+/**
+ * 트리가 주어진다.
+ * 첫 줄에는 정점의 개수 V기 주어진다. (2<= V <= 100000)
+ *
+ * 두번째 줄부턴 출발정점, 도착정점 둘사이의거리, 도착정점 둘사이의거리 ... -1이 나온다.
+ * 즉, 연결된 정점과 간선이 주어지며, 끝나면 -1이 나온다. (거리는 10000 이하의 자연수)
+ *
+ * 첫째 줄에 트리의 지름 (=둘 사이에 가장 긴 거리)
+ */
+public class no1167 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        bw.close();
+    }
+}
+
