bc1ff4cecd8bcc71503c91aaac3ba84b4b287276
Novice_Neoguri <nyong9221@gmail.com>
2023-06-11 04:29:08 +0900

    Solved: no1167 - 트리의지름

--- class1/src/no1167.java
+++ class1/src/no1167.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -13,13 +15,80 @@ import java.util.StringTokenizer;
  */
 public class no1167 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int V, Max, node;
+    private static List<Node>[] map;
+    private static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
+        V = Integer.parseInt(br.readLine());
+        Max = Integer.MIN_VALUE;
+        map = new ArrayList[V+1];
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        /**
+         * 정점의 조건 = 1부터 시작하는 2 이상 100000이하의 수
+         */
+        for(int i=1; i<=V; i++) map[i] = new ArrayList<>();
 
-        bw.close();
+        for(int i=1; i<=V; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int startNode = Integer.parseInt(st.nextToken());
+            while(true) {
+                int endNode = Integer.parseInt(st.nextToken());
+                if(endNode==-1) break;
+                int cost = Integer.parseInt(st.nextToken());
+                map[startNode].add(new Node(endNode, cost));
+            }
+        }
+
+        /**
+         * 모든 Node를 하나씩 지정해서, 각 노드에서 출발하여 말단까지 도달할 수 있는 모든 경우를 탐색할 것임.
+         */
+//        for(int start = 1 ; start <= V; start++) {
+//            visit = new boolean[V+1]; // 각 DFS 순회 당, 방문체크 배열을 만들어줌
+//            visit[start] = true;
+//            DFS(start, 0); //
+//        }
+
+        visit = new boolean[V+1];
+        DFS(1,0);
+
+        visit = new boolean[V+1];
+        DFS(node,0);
+
+
+        System.out.println(Max);
+    }
+
+    /**
+     * 처음 시작한 Node를 밑으로 갈 수 있는 모든 경로를 이동하며 이동경로당 비용을 sum으로 취합함.
+     * 이후, 만약 도달한 지점이 최댓값보다 크다면, 최댓값을 업데이트해줌
+     * @param departure 맨 처음에는 시작 Node이며, 재귀중에는 경로상의 직전에 이동한 하위 노드
+     * @param sum 이동하면서 취합한 비용의 합
+     */
+    private static void DFS (int departure, int sum) {
+        if(sum>Max) {
+            Max = sum;
+            node = departure;
+        }
+        visit[departure] = true; //
+
+        // 현재 도달한 Node에서 이동가능한 경로를 모두 탐색하되, 이번 재귀중에 방문하지 않았던 Node에 한해서만 탐색 진행.
+        for(Node next : map[departure]) {
+            if(!visit[next.Num]) {
+                visit[next.Num] = true;
+                DFS(next.Num, sum + next.Cost);
+            }
+        }
+    }
+
+    private static class Node {
+        private int Num, Cost;
+
+        public Node (int num, int cost) {
+            this.Num = num;
+            this.Cost = cost;
+        }
     }
 }
 
