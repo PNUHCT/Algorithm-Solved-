dfc7f6ccabf17853be5f9aaebf90b98259a71f7a
= <=>
2023-06-15 13:46:05 +0900

    no11725-BFS

--- class1/src/no11725.java
+++ class1/src/no11725.java
@@ -1,14 +1,13 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class no11725 {
-    private static boolean[][] map;
+    private static int N;
+    private static boolean[][] map, visit;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         map = new boolean[N+1][N+1];
         for(int i=0 ; i<N-1 ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -17,20 +16,50 @@ public class no11725 {
             map[low][cul] = true;
         }
 
-        OUTER : for(int i=2 ; i<=N ; i++) {
-            for(int j=1 ; j<=N ; j++) {
-                if(map[i][j]) {
-                    bw.write(""+j+"\n");
-                    continue OUTER;
+        for(int goal=1 ; goal<=N ; goal++) BFS(goal);
+
+        bw.close();
+    }
+
+    private static void BFS (int goal) {
+        Deque<Node> dq = new ArrayDeque<>();
+        visit = new boolean[N+1][N+1];
+        for(int i=2 ; i<=N ; i++) {
+            for(int j=2 ; j<=N ; j++) {
+                if(i==1 && map[i][j]) {
+                    dq.add(new Node(1, j));
+                    visit[1][j] = true;
                 }
-                if(map[j][i]) {
-                    bw.write(""+j+"\n");
-                    continue OUTER;
+                if(j==1 && map[i][j]) {
+                    dq.add(new Node(1, i));
+                    visit[1][i] = true;
                 }
             }
         }
 
-        bw.close();
+        while(!dq.isEmpty()) {
+            Node now = dq.poll();
+            if(now.Child==goal) bw.write(""+now.Parent+"\n");
+            break;
+
+            for(int next=1 ; next<=N ; next++) {
+                if(map[now.Child][next]) {
+                    visit[now.Child][next] = true;
+                    dq.add(new Node(now.Child, next));
+                }
+                if(map[next][now.Child]) {
+                    visit[next][now.Child] = true;
+                    dq.add(new Node(now.Child, next));
+                }
+            }
+        }    
+    }
+
+    private static class Node {
+        private int Parent, Child;
+        public Node(int parent, int child) {
+            this.Parent = parent;
+            this.Child = child;
+        }
     }
 }
-
