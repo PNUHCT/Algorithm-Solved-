dfb0755e3ea22ef18ef14b43de7bdba951aefe47
Novice_Neoguri <nyong9221@gmail.com>
2023-06-16 23:46:53 +0900

    [Bronze IV] Title: !밀비 급일, Time: 128 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            String code = br.readLine();
+            if (code.equals("END")) break;
+            for (int i = code.length() - 1; i >= 0; i--) bw.write(code.charAt(i));
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
