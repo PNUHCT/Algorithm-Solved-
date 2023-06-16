f2864faac35dd7016ce9b8eaafd2f150331f9b3a
= <=>
2023-06-16 17:37:39 +0900

    no9663 - 초기화

---
+++ class1/src/no9663.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class no9663 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        
+
+    }
+}
