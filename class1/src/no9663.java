cf02ef1e22982b7830cfde4087d8d287361cf9fa
Novice_Neoguri <nyong9221@gmail.com>
2023-06-17 00:09:48 +0900

    no9663 - 재귀호출

--- class1/src/no9663.java
+++ class1/src/no9663.java
@@ -1,11 +1,39 @@
 import java.io.*;
-import java.util.*;
 
 public class no9663 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    private static int N, count;
+    private static int[] arr;
     public static void main(String[] args) throws IOException {
-        
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
+        check(0); // check의 int형 변수 = 0~N번째으 퀸
+
+        System.out.println(count);
+    }
+
+    private static void check(int nQueen) {
+        // N개를 채운 상태면 count 증가 후 스답
+        if(nQueen == N) {
+            count++;
+            return;
+        }
+
+        for(int i = 0 ; i < N ; i++) { // N개까지 확인할 것이며,
+            arr[nQueen] = i;
+            if(possibility(nQueen)) check(nQueen + 1); // N개 중 현재 순서의 퀸을 놓을 수 있다면, 다음 퀀 놓을자리를 다시 체크
+        }
+    }
+
+    /**
+     * 상하좌우 방향의 모든 칸에 대해서 가능/불가능 확인 용도의 메소드
+     */
+    private static boolean possibility(int column) {
+        for(int now = 0 ; now < column ; now++) {
+            if(arr[column] == arr[now]) return false;
+            else if (Math.abs(column - now) == Math.abs(arr[column] - arr[now])) return false;
+        }
+        return  true;
     }
 }
