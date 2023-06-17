f04b6ca69c00a0e73f2f3eea39cdbeebcbc0d7b2
Novice_Neoguri <nyong9221@gmail.com>
2023-06-18 02:31:03 +0900

    no13866

---
+++ class1/src/no13866.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class no13866 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[4];
+        for(int i=0 ; i<4 ; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        System.out.println(Math.abs((arr[0] + arr[3]) - (arr[1]+arr[2])));
+    }
+}
