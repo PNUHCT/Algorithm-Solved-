546922b1616ddd58fc6633e2b83d692005f81a65
Novice_Neoguri <nyong9221@gmail.com>
2023-06-18 22:46:40 +0900

    no13752

---
+++ class1/src/no13752.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class no13752 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        while (N-->0) {
+            int now = Integer.parseInt(br.readLine());
+            for (int i = 0; i < now; i++) bw.append("=");
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
