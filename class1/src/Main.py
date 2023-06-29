f62de6880a78c9d44e9bbc4df9ce3f26a5ff583a
neoguri <nyong9221@gmail.com>
2023-06-29 17:49:30 +0900

    파이썬 템플릿 초기화

--- class1/src/Main.py
+++ class1/src/Main.py
@@ -1,21 +1,4 @@
-# 반복문을 사용한 합산
-# while 1 :
-#     N = int(input())
-#     if N == 0 : 
-#         break
-#     Result = 0
-#     for i in range(1, N+1):
-#         Result += i
-#     print(Result)
+N = int(input())
 
-#  N * (N+1) / 2 공식을 이용한 합산
-def Pyramid(num):
-    return num * (num+1) // 2
-
-# if __name__ == "__main__": 
-while 1:
-    N = int(input())
-    if N == 0 : 
-        break
-
-    print(Pyramid(num = N))
+for i in range(0, N) :
+    print(i)
