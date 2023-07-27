7a5d243885d3ab647287c3f8cfb96151c6e87e7d
Novice_Neoguri <nyong9221@gmail.com>
2023-07-27 18:50:00 +0900

    Solved: no17070 - BFS 코드(로컬 통과)

--- class1/src/no17070.java
+++ class1/src/no17070.java
@@ -3,30 +3,8 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-
-/**
- * 문제 링크
- * https://www.acmicpc.net/problem/17070
- *
- * 조건
- * 1. 파이프는 무조건 두 칸을 차지한다
- * 2. 첫 파이프는 항상 (1,1)(1,2)에 걸쳐있다 (길이가 2칸인 파이프)
- * 3. 파이프의 이동은 다음의 이동만 가능하다.
- *    가로일 때, 가로 or 오른쪽아래대각선
- *    세로일 때, 세로 or 오른쪽아래대각선
- *    대각선일 때, 가로 or 세로 or 오른쪽아래대각선
- * 4. 목표하는 N,N까지 이동이 가능한 경우의 수를 구해야 한다.
- * 5. 단, 중간에 이동이 불가능한 1인 칸이 있다면, 해당 경로는 지나칠 수 없다.
- * 6. 만약 대각선 이등을 해야한다면, 총 네칸을 차지한다.
- *
- * Input
- * 1. 사이즈 N
- * 2. 맵 현황 (0은 이동가느한 빈칸, 1은 이동불가한 벽)
- */
-
 public class no17070 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static int N;
     private static boolean[][] field;
 
@@ -46,9 +24,10 @@ public class no17070 {
 
         N = Integer.parseInt(br.readLine());
         field = new boolean[N+1][N+1];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st;
 
         for(int i = 1; i<= N; i++) {
+           st = new StringTokenizer(br.readLine(), " ");
            for(int j=1 ; j<= N; j++) {
                if(st.nextToken().equals("1")) field[i][j] = true;
            }
@@ -56,47 +35,73 @@ public class no17070 {
 
         field[1][1] = field[1][2] = true;
 
-        bw.close();
+        int result = BFS();
+
+        System.out.println(result);
     }
 
-    private static void BFS() {
+    private static int BFS() {
         Deque<Node> dq = new ArrayDeque<>();
         // 처음은 무조건 가로이므로, 가로 방향에서 가능한 이동방식으로 초기화
-        for(int i=0 ; i<horWidth.length ; i++) {
-            dq.add(new Node(1+verWidth[i], 2+horWidth[i], i)); // 가로 0 대각선 1
+        if(field[1][3]) return 0;
+
+        if(field[2][2]) dq.add(new Node(1, 3, 0));
+        else {
+            for (int i = 0; i < horWidth.length; i++) {
+                dq.add(new Node(1 + verWidth[i], 2 + horWidth[i], i)); // 가로 0 대각선 1
+            }
         }
 
-        /**
-         * 1. dq를 반복하며,
-         * 2. 이전 상태가 가로일 때, 가로 조건으로 돌아
-         * 3. 이전 상태가 세로일 때, 세로 조건으로 돌아
-         * 4. 이전 상태가 대각선일 때, 대각선 조건으로 돌아
-         */
+        int count = 0;
 
         while (!dq.isEmpty()) {
             Node now = dq.poll();
 
+            if(now.Y==N && now.X==N) count++;
+
             // 가로
             if(now.NodeType==0) {
                 for(int i=0 ; i<verWidth.length ; i++) {
-                    if(i==0) {
-
+                    if(i==0) {  // 가로 이동
+                        if(now.X+1<=N &&
+                                !field[now.Y][now.X+1]) dq.add(new Node(now.Y + verWidth[i], now.X + horWidth[i], 0));
                     }
-                    else {
-                        if(field[now.Y][now.X+1] && field[now.Y+1][now.X] && field[now.Y+1][now.X+1]) dq.add(new Node(now.Y+1, now.X+1, 1));
+                    else { // 대각선 이동 - 조건 : 4칸이 true
+                        if(now.Y+1<=N && now.X+1 <= N &&
+                                !field[now.Y][now.X+1] && !field[now.Y+1][now.X] && !field[now.Y+1][now.X+1]) dq.add(new Node(now.Y + verWidth[i], now.X + horWidth[i], 1));
                     }
                 }
             }
             // 대각선
             else if(now.NodeType==1) {
-
+                for(int i=0 ; i<verDiagonal.length ; i++ ) {
+                    if(i==0) { // 가로이동
+                        if(now.X+1 <= N &&
+                                !field[now.Y][now.X+1]) dq.add(new Node(now.Y + verDiagonal[i], now.X + horDiagonal[i], 0));
+                    } else if (i==1) { // 세로이동
+                        if(now.Y+1<=N &&
+                                !field[now.Y+1][now.X]) dq.add(new Node(now.Y + verDiagonal[i], now.X + horDiagonal[i], 2));
+                    } else { // 대각선 이동
+                        if(now.Y+1<=N && now.X+1 <= N &&
+                                !field[now.Y][now.X+1] && !field[now.Y+1][now.X] && !field[now.Y+1][now.X+1]) dq.add(new Node(now.Y + verDiagonal[i], now.X + horDiagonal[i], 1));
+                    }
+                }
             }
             // 세로
             else {
-
+                for(int i=0 ; i<verLength.length ; i++) {
+                    if(i==0) {  // 세로 이동
+                        if(now.Y+1 <= N &&
+                                !field[now.Y+1][now.X]) dq.add(new Node(now.Y + verLength[i], now.X + horLength[i], 2));
+                    }
+                    else { // 대각선 이동 - 조건 : 4칸이 true
+                        if(now.Y+1<=N && now.X+1 <= N &&
+                                !field[now.Y][now.X+1] && !field[now.Y+1][now.X] && !field[now.Y+1][now.X+1]) dq.add(new Node(now.Y + verLength[i], now.X + horLength[i], 1));
+                    }
+                }
             }
-
         }
+        return count;
     }
 
     private static class Node {
@@ -113,4 +118,3 @@ public class no17070 {
         }
     }
 }
-
