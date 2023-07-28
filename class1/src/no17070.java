e9674c4dcf32fc061d0976834f07093a50a5fa7a
Novice_Neoguri <nyong9221@gmail.com>
2023-07-28 21:06:16 +0900

    Solved: no17070 - DP (3차원 배열 이용 상태관리)

--- class1/src/no17070.java
+++ class1/src/no17070.java
@@ -6,115 +6,52 @@ import java.util.StringTokenizer;
 public class no17070 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int N;
+    /**
+     * field : 입력받은 맵 형태로서, true는 곧 벽이 있다는 표시다.
+     */
     private static boolean[][] field;
-
-    // 현재상태가 가로일 때
-    private static int[] verWidth = {0, 1};
-    private static int[] horWidth = {1, 1};
-
-    // 현재 상태가 세로일 때
-    private static int[] verLength = {1, 1};
-    private static int[] horLength = {0, 1};
-
-    // 현재 상태가 대각선일 때
-    private static int[] verDiagonal = {0, 1, 1};
-    private static int[] horDiagonal = {1, 0, 1};
+    /**
+     * DP
+     * : 가로, 세로, 대각선이 가능 한 경우를 모두 등록하기위한 3차원 배열
+     * : 1, 2차원은 맵을 나타내며, 3차원은 각각 0은 가로, 1은 세로, 2는 대각선인 상태를 저장하는 방식이다.
+     */
+    private static int[][][] DP;
 
     public static void main(String[] args) throws IOException {
 
+        // 입력구간
         N = Integer.parseInt(br.readLine());
-        field = new boolean[N+1][N+1];
+        field = new boolean[N][N];
+        DP = new int[N][N][3];
         StringTokenizer st;
-
-        for(int i = 1; i<= N; i++) {
-           st = new StringTokenizer(br.readLine(), " ");
-           for(int j=1 ; j<= N; j++) {
-               if(st.nextToken().equals("1")) field[i][j] = true;
-           }
+        for(int i=0 ; i<N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0 ; j<N; j++) {
+                if(st.nextToken().equals("1")) field[i][j] = true;
+            }
         }
 
-        field[1][1] = field[1][2] = true;
 
-        int result = BFS();
+        DP[0][1][0] = 1; // 초기화
+
+        // N * N을 순회하며, 구할 수 있는 경우를 순차적으로 칸마다 저장
+        // 최종적으로 마지막 칸에 도달한 가로/세로/대각선은 각각의 구할 수 있는 경우의 수 합임
+        for(int i=0 ; i<N ; i++) {
+            for(int j=2 ; j<N ; j++) {
+                // 가로일 때
+                if (j-1 >= 0 && !field[i][j]) DP[i][j][0] = DP[i][j-1][0] + DP[i][j-1][2];
+                // 세로일 때
+                if (i-1 >= 0 && !field[i][j]) DP[i][j][1] = DP[i-1][j][1] + DP[i-1][j][2];
+                // 대각선 일때
+                if (i-1 >= 0 && j-1 >= 0 && !field[i][j] && !field[i-1][j] && !field[i][j-1]) {
+                    DP[i][j][2] = DP[i-1][j-1][0] + DP[i-1][j-1][1] + DP[i-1][j-1][2];
+                };
+            }
+        }
+
+        // 최종 목적지에 도달할 때, 가로/세로/대각선으로 도달한 경우의 수를 모두 합치면 정답
+        int result = DP[N-1][N-1][0] + DP[N-1][N-1][1] + DP[N-1][N-1][2];
 
         System.out.println(result);
-    }
-
-    private static int BFS() {
-        Deque<Node> dq = new ArrayDeque<>();
-        // 처음은 무조건 가로이므로, 가로 방향에서 가능한 이동방식으로 초기화
-        if(field[1][3]) return 0;
-
-        if(field[2][2]) dq.add(new Node(1, 3, 0));
-        else {
-            for (int i = 0; i < horWidth.length; i++) {
-                dq.add(new Node(1 + verWidth[i], 2 + horWidth[i], i)); // 가로 0 대각선 1
-            }
-        }
-
-        int count = 0;
-
-        while (!dq.isEmpty()) {
-            Node now = dq.poll();
-
-            if(now.Y==N && now.X==N) count++;
-
-            // 가로
-            if(now.NodeType==0) {
-                for(int i=0 ; i<verWidth.length ; i++) {
-                    if(i==0) {  // 가로 이동
-                        if(now.X+1<=N &&
-                                !field[now.Y][now.X+1]) dq.add(new Node(now.Y + verWidth[i], now.X + horWidth[i], 0));
-                    }
-                    else { // 대각선 이동 - 조건 : 4칸이 true
-                        if(now.Y+1<=N && now.X+1 <= N &&
-                                !field[now.Y][now.X+1] && !field[now.Y+1][now.X] && !field[now.Y+1][now.X+1]) dq.add(new Node(now.Y + verWidth[i], now.X + horWidth[i], 1));
-                    }
-                }
-            }
-            // 대각선
-            else if(now.NodeType==1) {
-                for(int i=0 ; i<verDiagonal.length ; i++ ) {
-                    if(i==0) { // 가로이동
-                        if(now.X+1 <= N &&
-                                !field[now.Y][now.X+1]) dq.add(new Node(now.Y + verDiagonal[i], now.X + horDiagonal[i], 0));
-                    } else if (i==1) { // 세로이동
-                        if(now.Y+1<=N &&
-                                !field[now.Y+1][now.X]) dq.add(new Node(now.Y + verDiagonal[i], now.X + horDiagonal[i], 2));
-                    } else { // 대각선 이동
-                        if(now.Y+1<=N && now.X+1 <= N &&
-                                !field[now.Y][now.X+1] && !field[now.Y+1][now.X] && !field[now.Y+1][now.X+1]) dq.add(new Node(now.Y + verDiagonal[i], now.X + horDiagonal[i], 1));
-                    }
-                }
-            }
-            // 세로
-            else {
-                for(int i=0 ; i<verLength.length ; i++) {
-                    if(i==0) {  // 세로 이동
-                        if(now.Y+1 <= N &&
-                                !field[now.Y+1][now.X]) dq.add(new Node(now.Y + verLength[i], now.X + horLength[i], 2));
-                    }
-                    else { // 대각선 이동 - 조건 : 4칸이 true
-                        if(now.Y+1<=N && now.X+1 <= N &&
-                                !field[now.Y][now.X+1] && !field[now.Y+1][now.X] && !field[now.Y+1][now.X+1]) dq.add(new Node(now.Y + verLength[i], now.X + horLength[i], 1));
-                    }
-                }
-            }
-        }
-        return count;
-    }
-
-    private static class Node {
-        private int Y, X, NodeType;
-
-        private enum NodeType {
-            width, diagonal, length   // 0, 1, 2
-        }
-
-        public Node(int y, int x, int nodeType) {
-            this.Y = y;
-            this.X = x;
-            this.NodeType = nodeType;
-        }
     }
 }
