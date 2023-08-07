de1370b286452e2942e1654ff02e301213557709
Novice_Neoguri <nyong9221@gmail.com>
2023-08-07 22:54:49 +0900

    Solved: no9935 - stack 성공

--- class1/src/no9935.java
+++ class1/src/no9935.java
@@ -1,10 +1,11 @@
 import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Stack;
 
 public class no9935 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static Deque<Character> dq, clipBoard;
+    private static Stack<Character> dq;
     private static String str, boom;
 
     public static void main(String[] args) throws IOException {
@@ -12,64 +13,51 @@ public class no9935 {
         str = br.readLine();
         // 필터링 할 단어
         boom = br.readLine();
+        int size = boom.length();
         // 남는 단어 넣을 기본 배열
-        dq = new ArrayDeque<>();
+        dq = new Stack<>();
         /* 비교군 비울 배열
         1. 필터링 할 단어의 역순으로 순차적으로 비교해서 일치하면 담음
         2. 만약 모두 일치하면 비우기
         3. 만약 중간에 일치하지 않으면 다시 원위치 시킴
         */
-        clipBoard = new ArrayDeque<>();
-        char last = boom.charAt(boom.length() - 1);
 
-        // 비교용 문자열이 1자리일 경우, 그것만 다 지워줌
+
+        // 비교용 문자열이 1자리일 경우, 그것만 다 지워주고 출력 (끝)
         if(boom.length()==1) {
             str = str.replaceAll(boom, "");
             System.out.println(str);
         }
+        // 그 외의 경우
         else {
-            int now = 0;
-            while (now < str.length()) {
-                // dq에 남은 문자가 비교용 단어의 길이보다 짧을 때, 최소한의 비교할 만큼의 문자를 채워줌
-                if (dq.size() < boom.length() - 1) {
-                    while (dq.size() != boom.length()-1) {
-                        dq.add(str.charAt(now));
-                        now++;
-                    }
-                    continue;
-                }
+            for(int i=0; i<str.length(); i++) {
+                dq.push(str.charAt(i));
 
-                char nowChar = str.charAt(now);
-                now++;
+                // 폭발 문자열과 길이가 같아지면 탐색 시작
+                if(dq.size()>= size) {
+                    boolean flag = true;
 
-                // 마지막 문자와 같은 것이 걸렸을 때,
-                if (nowChar == last) {
-                    clipBoard.push(nowChar);
-                    for (int j = boom.length() - 2; j >= 0; j--) {
-                        // 비교해서 일치하면 클립보드에 넣어줌
-                        if (dq.peekLast() == boom.charAt(j)) {
-                            clipBoard.addLast(dq.pollLast());
-                            if(j==0) clipBoard = new ArrayDeque<>();
-                        }
-                        // 비교해서 일치하지 않으면, 클립보드에 있는애들 다 원상복귀 후, nowChar도 넣어줌
-                        else {
-                            while(!clipBoard.isEmpty()) dq.addLast(clipBoard.pollLast());
+                    // st.size-regexSize부터 ~ st.size까지 탐색하여 regex와 일치하면 제거
+                    for(int j=0; j<size; j++) {
+                        if(dq.get(dq.size()-size+j) != boom.charAt(j)) {
+                            flag = false;
                             break;
                         }
                     }
+                    if(flag) {
+                        for(int j=0; j<size; j++) {
+                            dq.pop();
+                        }
+                    }
                 }
-                else {
-                    dq.add(nowChar);
-                }
-            }
-            String result = "";
-            int size = dq.size();
-            for(int i=0 ; i<size ; i++) {
-                result += dq.poll();
+
             }
 
-            if(result.equals("")) System.out.println("FRULA");
-            else System.out.println(result);
+            StringBuilder sb = new StringBuilder();
+            for(Character c : dq) {
+                sb.append(c);
+            }
+            System.out.println(sb.length()==0? "FRULA" : sb.toString());
         }
     }
 }
