60411f1143261c3fcf323640a7e1ed97cbad6570
Novice_Neoguri <nyong9221@gmail.com>
2023-08-10 18:26:31 +0900

    Solved: no11779 - 로컬성공

--- class1/src/no11779.java
+++ class1/src/no11779.java
@@ -5,8 +5,7 @@ public class no11779 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static int city_amount, bus_amount, min;
-//    private static List<ArrayList<Integer>> map;
-    private static Integer[][] map, cost_map;
+    private static Integer[][] map;
     private static StringTokenizer st;
     private static List<Integer> via;
 
@@ -15,9 +14,7 @@ public class no11779 {
         city_amount = Integer.parseInt(br.readLine());
         bus_amount = Integer.parseInt(br.readLine());
 
-//        for(int i = 0; i<= city_amount; i++) map.add(new ArrayList<>());
         map = new Integer[city_amount+1][city_amount+1];
-        cost_map = new Integer[city_amount+1][city_amount+1];
         for(int i = 0; i< bus_amount; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
@@ -31,7 +28,7 @@ public class no11779 {
         int B = Integer.parseInt(st.nextToken());
 
         // 결과 입력용 변수 초기화
-        min = 0;
+        min = Integer.MAX_VALUE;
         via = new ArrayList<>();
 
         dijkstra(A, B);
@@ -47,6 +44,7 @@ public class no11779 {
         PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
         for(int i = 1 ; i <= city_amount ; i++) {
             ArrayList<Integer> list = new ArrayList<>();
+            list.add(A);
             list.add(i);
             if(map[A][i]!=null) pq.add(new Node(i, map[A][i], list));
         }
@@ -54,16 +52,18 @@ public class no11779 {
 
         while(!pq.isEmpty()) {
             Node now = pq.poll();
+            ArrayList<Integer> list = now.list;
 
             if(now.city==B) {
-                min = now.cost;
-                via = now.list;
+                min = Math.min(min,now.cost);
+                via = list;
                 break;
             }
 
-            for(int i=0 ; i< city_amount ; i++) {
+            for(int i=1 ; i<= city_amount ; i++) {
                 if(map[now.city][i]!=null) {
-                    ArrayList<Integer> now_list = now.list;
+                    ArrayList<Integer> now_list = new ArrayList<>();
+                    now_list.addAll(list);
                     now_list.add(i);
                     pq.add(new Node(i, now.cost+map[now.city][i], now_list));
                 }
