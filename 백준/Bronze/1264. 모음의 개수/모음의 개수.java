cdbe1a9341b94581bf2a1104df1ca6eb081cab24
Novice_Neoguri <nyong9221@gmail.com>
2023-08-16 00:18:20 +0900

    [Bronze IV] Title: 모음의 개수, Time: 140 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -1,26 +1,21 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static ArrayList<String> list = new ArrayList<>(Arrays.asList("a", "e", "i", "o", "u"));
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        List<Character> charList = new ArrayList<>();
-        charList.add('a');charList.add('e');charList.add('i');charList.add('o');charList.add('u');
-        charList.add('A');charList.add('E');charList.add('I');charList.add('O');charList.add('U');
-        
-        while(true) {
-            String st = br.readLine();
-            if(st.equals("#")) break;
-            int cnt = 0;
-            for(int i=0 ; i<st.length() ; i++) if(charList.contains(st.charAt(i))) cnt++;
+        while (true) {
+            String str = br.readLine().toLowerCase();
+            if (str.equals("#")) break;
 
-            bw.write(""+cnt+"\n");
+            String[] line = str.split("");
+            int cnt = 0;
+            for (String cha : line) if (list.contains(cha)) cnt++;
+
+            System.out.println(cnt);
         }
-        bw.close();
     }
 }
