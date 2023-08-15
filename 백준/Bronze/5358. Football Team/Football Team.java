3ddaf74e55cede17d7693a251eb26af1eb226cb3
Novice_Neoguri <nyong9221@gmail.com>
2023-08-16 00:30:50 +0900

    [Bronze IV] Title: Football Team, Time: 132 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/5358. Football Team/Football Team.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
+
+        while (true){
+            try {
+                String[] str = br.readLine().split("");
+                String result = "";
+                for(String st : str) {
+                    if(st.equals("i")) result += "e";
+                    else if (st.equals("e")) result += "i";
+                    else if (st.equals("I")) result += "E";
+                    else if (st.equals("E")) result += "I";
+                    else result += st;
+                }
+                System.out.println(result);
+            } catch (Exception e) {
+                break;
+            }
+
+        }
+    }
+}
