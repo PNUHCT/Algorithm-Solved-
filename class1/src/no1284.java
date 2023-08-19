0a8fd25229e641b9724e016bf032cc19f1fe101e
Novice_Neoguri <nyong9221@gmail.com>
2023-08-20 02:25:00 +0900

    no1284

---
+++ class1/src/no1284.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class no1284 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static String T;
+
+    public static void main(String[] args) throws IOException {
+
+        while (true) {
+            T= br.readLine();
+
+            if(T.equals("0")) break; // 베이스 코드
+
+            String[] arr = T.split("");
+
+            int sum = 1; // 첫 공백 포함
+            for(String s : arr) {
+                if(s.equals("1")) sum += 3;  // 2 + 1
+                else if(s.equals("0")) sum += 5;  // 4 + 1
+                else sum += 4;  // 3 + 1
+            }
+            bw.write(sum+"\n");
+        }
+
+        bw.close();
+    }
+}
+
---
+++ class1/src/no2083.py
@@ -0,0 +1,49 @@
+# StringTokenizer for Python
+
+class StringTokenizer:
+    def __init__(self, string, delimiters):
+        self.string = string
+        self.delimiters = delimiters
+        self.tokens = self.tokenize()
+
+    def tokenize(self):
+        token_list = []
+        current_token = ""
+        for char in self.string:
+            if char in self.delimiters:
+                if current_token:
+                    token_list.append(current_token)
+                    current_token = ""
+            else:
+                current_token += char
+        if current_token:
+            token_list.append(current_token)
+        return token_list
+
+    def count_tokens(self):
+        return len(self.tokens)
+
+    def get_token(self, index):
+        if index < 0 or index >= len(self.tokens):
+            return None
+        return self.tokens[index]
+
+
+while 1 :
+    delimiters = " "
+    tokenizer = StringTokenizer(input(), delimiters)
+
+    name = tokenizer.get_token(0)
+
+    if name == "#" : break
+    if int(tokenizer.get_token(1)) >17 or int(tokenizer.get_token(2)) >=80 : print(name + " Senior")
+    else : print(name + " Junior")
+
+# -------------------------------------------------------------------------------------------------
+# 단순 split()
+# while 1 :
+    # name, age, weight = input().split()
+    # if '#' in name : break
+
+    # if int(age)>17 or int(weight)>=80 : print(f"{name} Senior")
+    # else : print(f"{name} Junior")
