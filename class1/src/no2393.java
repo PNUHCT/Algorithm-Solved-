4069af4536a362d6ed2486a9b9f8e66fe83c065d
Novice_Neoguri <nyong9221@gmail.com>
2023-08-27 15:51:58 +0900

    Solved: no2393 - 룩

---
+++ class1/src/no2393.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class no2393 {
+    public static void main(String[] args) throws IOException {
+
+        String output = "  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\";
+
+        System.out.println(output);
+    }
+}
+
