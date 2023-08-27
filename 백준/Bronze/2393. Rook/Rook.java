8ad86fabde00c7250383dbdebbb601c2e304e15c
Novice_Neoguri <nyong9221@gmail.com>
2023-08-27 15:51:43 +0900

    [Bronze V] Title: Rook, Time: 124 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String output = "  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\";
+        
+        System.out.println(output);
+    }
+}
