b91df62b7692ba2370aa8b24c7bbb598815c6f46
Novice_Neoguri <nyong9221@gmail.com>
2023-09-05 20:04:04 +0900

    [Bronze III] Title: 윷놀이, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        /**
+         * 0은 뒤집힌 것 == true
+         * 1은 안 뒤집힌 것 ==  false
+         *
+         * 0이 1개 도
+         * 0이 2개 개
+         * 0이 3개 걸
+         * 0이 4개 윳
+         * 0이 0개 모
+         */
+        int round = 3;
+        while (round-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int cnt = 0;
+
+            for (int i = 0; i < 4; i++) if (st.nextToken().equals("0")) cnt++;
+
+            switch (cnt) {
+                case 0:
+                    System.out.println("E");
+                    break;
+                case 1:
+                    System.out.println("A");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("C");
+                    break;
+                case 4:
+                    System.out.println("D");
+                    break;
+            }
+        }
+    }
+}
