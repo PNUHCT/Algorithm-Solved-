2612fbccb1b3a732a1f3b68f395b4acc4f084460
PNUHCT <nyong9221@gmail.com>
2023-09-17 15:57:13 +0900

    no13623

--- class1/src/no13623.java
+++ class1/src/no13623.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class no13623 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static String A, B ,C;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
