af1c4a9deab5b258d8afa70f98521b9a3cd67c60
PNUHCT <nyong9221@gmail.com>
2023-09-17 16:03:25 +0900

    commit test

---
+++ class1/src/no5522.java
@@ -0,0 +1,16 @@
+    import java.io.*;
+    import java.util.StringTokenizer;
+
+    public class no5522 {
+        private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public static void main(String[] args) throws IOException {
+
+            int cycle = 5;
+            int result = 0;
+            while(cycle-->0) result += Integer.parseInt(br.readLine());
+
+            System.out.println(result);
+        }
+    }
+
